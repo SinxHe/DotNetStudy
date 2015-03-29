23371ff6d047ecff174c5343cb2b439ffa9279b2
HeabKing <HeabKing@qq.com>
2015-03-29 16:08:18 +0800

    2015-3-29 16:07:07 泛型 拓展方法 委托 事件

---
+++ N03自定义泛型/Program.cs
@@ -0,0 +1,46 @@
+﻿// 2015-3-29 13:22:16
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N03自定义泛型
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass<int> mc = new MyClass<int>(new int[] {1, 2, 3, 4});
+            mc.Show();
+            Program p = new Program();
+            p.Show(mc);
+        }
+
+        public void Show<T>(T param)
+        {
+            //param.Show();// TODO:难道不能直接这么用?
+        }
+    }
+
+    /// <summary>
+    /// 泛型类
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class MyClass<T>
+    {
+        T[] names;
+        public MyClass(T[] names)
+        {
+            this.names = names;
+        }
+
+        public void Show()
+        {
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ N04拓展方法/N01拓展方法.cs
@@ -0,0 +1,47 @@
+﻿// 2015-3-29 14:01:21
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N04拓展方法
+{
+    class N01拓展方法
+    {
+        static void Mai(string[] args)
+        {
+            MyClass mc = new MyClass();
+            mc.Name = "HeabKing";
+            
+            // 类中方法
+            mc.SayHi();
+            // 拓展方法(有一个向下的小箭头)
+            mc.SayHello();
+        }
+    }
+
+    class MyClass
+    {
+        public string Name { get; set; }
+        public void SayHi()
+        {
+            Console.WriteLine("Hi, " + Name);
+        }
+    }
+
+    /// <summary>
+    /// 1.  增加一个静态类, 类名随便起;
+    /// 2.  该静态类应该与使用拓展方法的地方在一个命名空间下, 否则必须导入该命名空间
+    /// </summary>
+    static partial class MethodExt
+    {
+        // 3.   向静态类中添加静态方法
+        //      1.  第一个参数表示要给那个类型添加该拓展方法
+        //      2.  
+        public static void SayHello(this MyClass obj)
+        {
+            Console.WriteLine("Hello, " + obj.Name +", 我来自拓展方法!");
+        }
+    }
+}
